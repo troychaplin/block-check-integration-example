559edc1714da97bf8193af7905ec309a59cd6ba0
Troy Chaplin <troy.chaplin@gmail.com>
2025-11-02 08:11:34 -0500

    update: dependencies

--- build/blocks/album-card/index.asset.php
+++ build/blocks/album-card/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '69e4238f241b8a94a198');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'b550b6aa782be9d76f28');
