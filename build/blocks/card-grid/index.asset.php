f0042594d1173b0faab7fc945a45d2d85d61c7b7
Troy Chaplin <troy.chaplin@gmail.com>
2025-12-29 23:51:03 -0500

    change: block check function name

--- build/blocks/card-grid/index.asset.php
+++ build/blocks/card-grid/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => 'c6fd1b69aa1f08074a2e');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => '59ecfc30a5b02188d173');
